9a559f8a27d2f220455b88a27bcfef35eef5ec03
Anas Hashim <anashashim@shaibiginfosystem.com>
2018-02-12 19:54:41 +0530

    Update

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
+class PostIndex extends Component{
 
-export default ()=> {
-  return(
-    <div>Post</div>
-  )
+  componentWillMount(){
+    this.props.fetchPost();
+  }
+
+  render(){
+    return(
+      <div>All blog post</div>
+    )
+  }
 }
+
+export default connect(null, { fetchPost  })(PostIndex);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { all : [], post:null};
 export default funtion( state = INITIAL_STATE, action){
   switch (action.type) {
     case FETCH_POST:
-
+      return { ...state, all : action.payload.data }
       break;
     default:
       return state;
